2fff750096e4cb30b027efbbad546d00e9efd2c9
AlinaChernyshova1998 <alina.chernyshova1998@gmail.com>
2019-08-06 20:14:40 -0400

    finishing third line

--- src/test/java/LogInTest.java
+++ src/test/java/LogInTest.java
@@ -2,6 +2,7 @@ public class LogInTest {
     public static void main(String[] args) {
         System.out.println("line 1");
         System.out.println("line 2");
+        System.out.println("line 3");
 
     }
 }
